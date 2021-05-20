143dc19120179e9525d31f97ab5a64320e67c578
0608434 <71476500+0608434@users.noreply.github.com>
2021-05-20 21:03:36 +0800

    攝影機

---
+++ 2D_Final_0608434/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    public Vector2 limitY = new Vector2(-5, 5);
+    public Vector2 limitX = new Vector2(-5, 5);
+
+
+}
--- 2D_Final_0608434/Assets/Scripts/Player.cs
+++ 2D_Final_0608434/Assets/Scripts/Player.cs
@@ -7,6 +7,41 @@ public class Player : MonoBehaviour
     public float speed = 10.5f;
     public bool isDead = false;
     public string cName = "主角";
-    
+
+    public FixedJoystick joystick;
+    public Transform tra;
+    public Animator ani;
+
+    private void Move ()
+    {
+
+        float h = joystick.Horizontal;
+        tra.Translate(h * speed * Time.deltaTime, 0, 0);
+        ani.SetFloat("水平",h);
+
+    }
+    private void Attack()
+    {
+
+    }
+    private void Hit()
+    {
+
+    }
+    private void Dead()
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
 
 }
