533bbed291e5bb0a82fdb55e3aea41005af0afea
0608434 <71476500+0608434@users.noreply.github.com>
2021-05-20 19:55:32 +0800

    虛擬搖桿

---
+++ 2D_Final_0608434/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Range(0,300)]
+    public float speed = 10.5f;
+    public bool isDead = false;
+    public string cName = "主角";
+    
+
+}
